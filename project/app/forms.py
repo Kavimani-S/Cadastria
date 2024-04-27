33b208b78eb7053651e187e6649d0aeccfa902c0
Kavimani-S <112235556+Kavimani-S@users.noreply.github.com>
2024-04-27 11:07:55 +0530

    Add files via upload

---
+++ project/app/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'genre', 'publication_year']
+        labels = {
+            'title': 'Title',
+            'author': 'Author',
+            'genre': 'Genre',
+            'publication_year': 'Publication Year'
+        }
+        widgets = { 
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'author': forms.TextInput(attrs={'class': 'form-control'}),
+            'genre': forms.TextInput(attrs={'class': 'form-control'}),
+            'publication_year': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
